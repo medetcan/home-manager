e294058c9ba753fa24859588d5fa49b63b9c4384
Medet Can Akus <medet@canakus.com>
2026-03-13 21:29:08 +0100

    Initial home-manager configuration

---
+++ home.nix
@@ -0,0 +1,136 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ./neovim.nix
+  ];
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "can";
+  home.homeDirectory = "/Users/can";
+  nixpkgs.config = {
+      allowUnfreePredicate = pkg:
+        builtins.elem (pkgs.lib.getName pkg) [
+         "claude-code"
+         "gemini-cli"
+        ];
+    };
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "25.11"; # Please read the comment before changing.
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = [
+    # # Adds the 'hello' command to your environment. It prints a friendly
+    # # "Hello, world!" when run.
+    pkgs.devenv
+    pkgs.gemini-cli
+    pkgs.claude-code
+    pkgs.secretspec
+    pkgs.nodejs
+    pkgs.nodePackages.yarn
+    pkgs.nodePackages.pnpm
+
+
+    # # It is sometimes useful to fine-tune packages, for example, by applying
+    # # overrides. You can do that directly here, just don't forget the
+    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+    # # fonts?
+    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+
+    # # You can also create simple shell scripts directly inside your
+    # # configuration. For example, this adds a command 'my-hello' to your
+    # # environment:
+    # (pkgs.writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
+  ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+    ".npmrc".text = "prefix=${config.home.homeDirectory}/.npm-global";
+  };
+
+  home.sessionPath = [
+    "${config.home.homeDirectory}/.npm-global/bin"
+  ];
+
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. These will be explicitly sourced when using a
+  # shell provided by Home Manager. If you don't want to manage your shell
+  # through Home Manager then you have to manually source 'hm-session-vars.sh'
+  # located at either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/can/etc/profile.d/hm-session-vars.sh
+  #
+  home.sessionVariables = {
+    # EDITOR = "emacs";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+  programs.bash.enable = true;
+  programs.bash.bashrcExtra = ''
+    export PATH="$HOME/develop/flutter/bin:$PATH";
+    export PATH="$PATH":"$HOME/.pub-cache/bin";
+  '';
+  programs.zsh.enable = true;
+  programs.zsh.autosuggestion.enable = true;
+
+  programs.zsh.initContent = ''
+    export PATH="$HOME/develop/flutter/bin:$PATH";
+    export PATH="$PATH":"$HOME/.pub-cache/bin";
+    bindkey -v;
+  '';
+  programs.direnv = {
+    enable = true;
+    enableBashIntegration = true;
+    enableZshIntegration = true;
+  };
+  programs.git = {
+    enable = true;
+    settings = {
+      user = {
+        email = "medet@canakus.com";
+        name = "Medet Can Akus";
+      };
+      init.defaultBranch = "main";
+      core = {
+        editor = "nvim";
+      };
+      pull = {
+        rebase = true;
+      };
+    };
+    ignores = [".devenv" ".direnv" "**/.claude/settings.local.json"];
+  };
+  programs.starship.enable = true;
+  programs.starship.enableBashIntegration = true;
+  programs.starship.enableZshIntegration = true;
+}
---
+++ neovim.nix
@@ -0,0 +1,42 @@
+{ config, pkgs, ... }:
+
+{
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+    plugins = with pkgs.vimPlugins; [
+      tokyonight-nvim
+      lualine-nvim
+      nvim-treesitter.withAllGrammars
+      plenary-nvim 
+      telescope-nvim
+    ];
+
+    # ALL Lua configuration grouped together safely
+    initLua = ''
+      vim.opt.number = true
+      vim.opt.relativenumber = true
+      vim.opt.expandtab = true
+      vim.opt.shiftwidth = 2
+      vim.opt.tabstop = 2
+      vim.opt.smartindent = true
+      vim.opt.termguicolors = true
+      
+      -- Load the colorscheme
+      vim.cmd[[colorscheme tokyonight]]
+
+      -- 2. Plugin Setups
+      require('lualine').setup { 
+        options = { theme = 'tokyonight' } 
+      }
+
+      local builtin = require('telescope.builtin')
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+      vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+      vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+    '';
+  };
+}
